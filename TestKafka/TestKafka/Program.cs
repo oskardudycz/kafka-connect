0bf39f919289d8d1478180fa5f268a0c1d629fa3
Oskar Dudycz <oskar.dudycz@gmail.com>
2018-08-26 18:41:13 +0200

    Added test kafka .net project and database initalisation

---
+++ TestKafka/TestKafka/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Confluent.Kafka;
+using Confluent.Kafka.Serialization;
+
+public class Program
+{
+    public static void Main()
+    {
+        var conf = new Dictionary<string, object>
+    {
+      { "group.id", "test-consumer-group" },
+      { "bootstrap.servers", "kafka:9092" },
+      { "auto.commit.interval.ms", 5000 },
+      { "auto.offset.reset", "earliest" }
+    };
+
+        using (var consumer = new Consumer<Null, string>(conf, null, new StringDeserializer(Encoding.UTF8)))
+        {
+            consumer.OnMessage += (_, msg)
+              => Console.WriteLine($"Read '{msg.Value}' from: {msg.TopicPartitionOffset}");
+
+            consumer.OnError += (_, error)
+              => Console.WriteLine($"Error: {error}");
+
+            consumer.OnConsumeError += (_, msg)
+              => Console.WriteLine($"Consume error ({msg.TopicPartitionOffset}): {msg.Error}");
+
+            consumer.Subscribe("dumb_table");
+
+            while (true)
+            {
+                consumer.Poll(TimeSpan.FromMilliseconds(100));
+            }
+        }
+    }
+}
--- docker/postgres/init.sql
+++ docker/postgres/init.sql
@@ -1,6 +1,4 @@
-CREATE DATABASE inventory;
 CREATE TABLE dumb_table(id SERIAL PRIMARY KEY, name VARCHAR);
 
 insert into dumb_table(name) values('oskar');
-
-SELECT id, name	FROM public.dumb_table;
+insert into dumb_table(name) values('test');
