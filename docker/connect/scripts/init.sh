48f4745bc8bde0a431fa2b1bf70393c8581797d1
Oskar Dudycz <oskar.dudycz@gmail.com>
2018-09-01 20:02:28 +0200

    added avro registry

--- docker/connect/scripts/init.sh
+++ docker/connect/scripts/init.sh
@@ -13,6 +13,12 @@ curl -X POST -H "Accept:application/json" -H "Content-Type:application/json" con
         "database.password": "postgres",
         "database.dbname": "postgres",
         "database.server.name": "dbserver1",
+        "key.converter": "io.confluent.connect.avro.AvroConverter",
+        "key.converter.schema.registry.url": "http://schema_registry:8081",
+        "value.converter": "io.confluent.connect.avro.AvroConverter",
+        "value.converter.schema.registry.url": "http://schema_registry:8081",
+        "internal.key.converter": "org.apache.kafka.connect.json.JsonConverter"
+        "internal.value.converter": "org.apache.kafka.connect.json.JsonConverter"
         "table.whitelist": "public.test_table,mt_doc_account,mt_doc_allaccountsummaryview,mt_doc_accountsummaryview,EventSourcingSample.mt_doc_clientview",
         "transforms": "route",
         "transforms.route.type": "org.apache.kafka.connect.transforms.RegexRouter",
@@ -48,6 +54,7 @@ curl -X POST -H "Accept:application/json" -H "Content-Type:application/json" con
         "transforms.key.type": "org.apache.kafka.connect.transforms.ExtractField$Key",
         "transforms.key.field": "id",
         "key.ignore": "false",
-        "type.name": "client"
+        "type.name": "client",
+        "compact.map.entries": "false"
     }
 }'
