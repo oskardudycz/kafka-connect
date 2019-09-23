1716c946c0aa51e6cca92bc9062d318b33ce2bfa
Oskar Dudycz <oskar.dudycz@gmail.com>
2019-09-23 21:58:59 +0200

    Next fixes

--- postgres/init.sql
+++ postgres/init.sql
@@ -39,5 +39,5 @@ CREATE TABLE testevents.mt_events
 
 INSERT INTO testevents.mt_events(
 	seq_id, id, stream_id, version, data, type, "timestamp", tenant_id, mt_dotnet_type)
-	VALUES (0, '1915d7de-3482-4d3a-b072-109e41e5cb0d', '9a0af90b-288b-454d-94e4-a44574748db6', 1, '{"Name": "Annie Hall"}', 'meeting_created', '2019-09-21 11:16:47.606273+00', '*DEFAULT*', 'Marten.WebApi.Meetings.Events.MeetingCreated, Marten.WebApi');
+	VALUES (0, '1915d7de-3482-4d3a-b072-109e41e5cb0d', '9a0af90b-288b-454d-94e4-a44574748db6', 1, '{"Name": "Annie Hall"}', 'meeting_created', '2019-09-21 11:16:47.606273+00', 'DEFAULT', 'Marten.WebApi.Meetings.Events.MeetingCreated, Marten.WebApi');
 
